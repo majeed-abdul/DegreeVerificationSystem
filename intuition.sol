5feed5eaca033de323ba473c3b036697babce453
majeed-abdul <majeedabdul.941485@gmail.com>
2023-10-30 17:47:59 +0500

    qwertyuilknb

--- intuition.sol
+++ intuition.sol
@@ -1,17 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-contract Intuition{
-    string key="ye6h4r47rrgw48f8wgfw6hrw79";
-
-    event push(string indexed sign);
-
-       
-    function upload(string memory signature)public {
-        emit push(signature);
-    }   
-    function uploadn()public {
-        emit push("signaturexxxxxxxxxxxxxxxxxxxxxxxxx");
-    }
-    
+contract Institution{
+    event push(string sign,string dataHash); 
+    mapping(string=>string) public records  ;     
+    function upload(string memory signature,string memory dataHash)public returns(string memory){
+        records[dataHash]=signature;
+        emit push(signature,dataHash);
+        return "done";
+    } function download(string memory dataHash)public view returns(string memory){ 
+        string memory sig=records[dataHash];
+        if(bytes(sig).length==0){
+            return "404";
+        }
+        return records[dataHash];
+    }    
 }
